a8fd4642441d239a80b4301ee8378900e5fc34e8
Oscar Cortez <om.cortez.2010@gmail.com>
2018-12-04 21:43:14 -0600

    Update json response

--- main.py
+++ main.py
@@ -1,21 +1,44 @@
-import aiohttp
-import asyncio
 import json
+
+from sanic import Sanic
+from sanic import response
+import aiohttp
 import requests
 from bs4 import BeautifulSoup
 
-async def fetch(session, url):
-    async with session.get(url) as response:
-        return await response.text()
+app = Sanic(__name__)
 
-async def scrappy():
-    async with aiohttp.ClientSession() as session:
-        html = await fetch(session, 'https://www.meetup.com/gdgmanagua/')
+
+async def scrappy(session):
+    async with session as response:
         req = requests.get('https://www.meetup.com/gdgmanagua/')
         content = req.content
-        soup = BeautifulSoup(content, "html.parser")
-        data = soup.find_all("script", {"data-react-helmet":"true"})[1]
-        return json.loads(data.text)
+        soup = BeautifulSoup(content, 'html.parser')
+        data = json.loads(soup.find_all('script', {'data-react-helmet': 'true'})[1].text)[0]
+        return {
+            'name': data['name'],
+            'start_date': data['startDate'],
+            'end_date': data['endDate'],
+            'description': data['description'],
+            'location': {
+                'name': data['location']['name'],
+                'lat': data['location']['geo']['latitude'],
+                'lon': data['location']['geo']['longitude']
+            }
+        }
 
-loop = asyncio.get_event_loop()
-loop.run_until_complete(scrappy())
+
+@app.route('/')
+async def handle_request(request):
+    async with aiohttp.ClientSession() as session:
+        data = await scrappy(session)
+        return response.json(
+            data,
+            headers={
+                'Access-Control-Allow-Origin': '*'
+            },
+        )
+
+
+if __name__ == '__main__':
+        app.run(host="0.0.0.0", port=8000)
